7fc3d0997cb714bde52cbefd21755e060de0f80f
zoya1610 <zoya16.info@gmail.com>
2026-04-29 20:25:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pickle
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import string
+
+# Initialize tools
+ps = PorterStemmer()
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    # Remove special characters, stopwords, and punctuation
+    y = [i for i in text if i.isalnum()]
+    y = [i for i in y if i not in stopwords.words('english') and i not in string.punctuation]
+    y = [ps.stem(i) for i in y]
+    return " ".join(y)
+
+# Load saved artifacts
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+st.title("Email/SMS Spam Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+    # 1. Preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. Vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. Predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
---
+++ setup.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/.nltk_data
+python -m nltk.downloader punkt
+python -m nltk.downloader stopwords
